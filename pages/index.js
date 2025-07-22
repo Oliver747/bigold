0b1aaa991d072224b0b4fa0c5f22b571f2ff3ec6
Oliver747 <37108566+Oliver747@users.noreply.github.com>
2025-07-22 14:04:35 +0800

    更新 index.js

--- pages/index.js
+++ pages/index.js
@@ -102,12 +102,13 @@ export default function Home() {
 
 const styles = {
   container: {
-    minHeight: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    background: "#f2f8fc",
-    fontFamily: "Segoe UI, sans-serif",
-  },
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  background: "linear-gradient(to bottom right, #4f46e5, #9333ea)", // 蓝到紫渐变
+  fontFamily: "Segoe UI, sans-serif",
+  color: "#ffffff", // 白色文字以适配深色背景
+},
   logo: {
     position: "absolute",
     top: 20,
